ffc80270da443a3db0b3dd2db81dedd0f350ac29
djjangid376 <44337910+djjangid376@users.noreply.github.com>
2022-12-16 22:16:30 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import WeatherDetails from "./WeatherDetails";
 
 function Card({ tempInfo }) {
   const [weatherState, setWeatherState] = useState("")
